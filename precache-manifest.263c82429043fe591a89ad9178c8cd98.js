d725428272b436bca7c5011c347f3b437d57774b
ARochniak <15bblinn@gmail.com>
2019-11-29 18:11:54 +0200

    Updates

---
+++ precache-manifest.263c82429043fe591a89ad9178c8cd98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd49e905318a8981424ad4c64b80bb9c",
+    "url": "/simpleStore/index.html"
+  },
+  {
+    "revision": "1fae88dac5ea3880af8d",
+    "url": "/simpleStore/static/css/main.e804d2a5.chunk.css"
+  },
+  {
+    "revision": "5a8eb0c0b249cd3a8e21",
+    "url": "/simpleStore/static/js/2.10f2a8bd.chunk.js"
+  },
+  {
+    "revision": "1fae88dac5ea3880af8d",
+    "url": "/simpleStore/static/js/main.47f82da5.chunk.js"
+  },
+  {
+    "revision": "bc9fabd440224f0438c3",
+    "url": "/simpleStore/static/js/runtime-main.7c6e1536.js"
+  }
+]);
